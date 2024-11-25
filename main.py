e9b5ecd9e88c1f2da9cc0506df5b189a167c48fc
masoudinia <81ali.masoud@gmail.com>
2024-11-26 00:23:06 +0330

    userloginadd

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def sayhi(name):
 def admin(username):
     if username == "admin":
         print("Welcome Admin")
+
+def username(user):
+    name = input("Enter your Username: ")
+    if user == name:
+        print("Logged In Successfully")
