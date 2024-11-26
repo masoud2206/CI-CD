5d32999a9ee22945d46c8171939e3820f233625c
masoudinia <81ali.masoud@gmail.com>
2024-11-26 03:38:06 +0330

    test added

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ def admin(username):
     if username == "admin":
         print("Welcome Admin")
 
-def username(user):
-    name = input("Enter your Username: ")
-    if user == name:
+def username(password):
+    if password == 1234:
         print("Logged In Successfully")
+
+
+def test_sayhi():
+    assert sayhi("ali") == "HI ali"
+
+
+
+def test_admin():
+    assert admin("admin") == "Welcome Admin"
+
+
+
+def test_username():
+    assert username(1234) == "Logged In Successfully"
